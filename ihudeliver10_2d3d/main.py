27b6d4714e712d263d80081b8637806f1b5d2ad3
coolteemf <francois.lecomte@inria.fr>
2024-07-08 14:17:07 +0200

    remove wrong roi2D_from_roi3D function

--- ihudeliver10_2d3d/main.py
+++ ihudeliver10_2d3d/main.py
@@ -8,9 +8,9 @@ import nibabel as nib
 from deepdrr import Volume, geo
 from skimage import io
 import matplotlib.pyplot as plt
-from utils import Visualize, check_disp_roi_invisible, disp_roi_from_img_roi, roi2D_from_roi3D, roi3D_from_center, roi3D_from_segmentation, \
+from utils import Visualize, disp_roi_from_img_roi, roi3D_from_center, roi3D_from_segmentation, \
                   load_volume_data, define_camera_matrix, project, crop_ddrr_volume, \
-                  crop_roi, crop_from_roi_2D, plot_img
+                  crop_roi, plot_img
 
 
 def main(volume_path: str, 
